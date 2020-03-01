9d0167a95d51c20936fbf9b50c18dd730a0057b6
Michael Hoyer <michaelhoyer@outlook.com>
2020-03-01 18:18:25 +0100

    Publishes in uint32. Need to be tested

--- voltage_publisher.py
+++ voltage_publisher.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import UInt32
 
 def publisher():
 
-    # TODO: change from String to int
+    # TODO: test conversion to UInt32
 
-    pub_volt_0 = rospy.Publisher('tx_volt_0', String, queue_size=10)
-    pub_volt_1 = rospy.Publisher('tx_volt_1', String, queue_size=10)
-    pub_volt_2 = rospy.Publisher('tx_volt_2', String, queue_size=10)
+    pub_volt_0 = rospy.Publisher('tx_volt_0', UInt32, queue_size=10)
+    pub_volt_1 = rospy.Publisher('tx_volt_1', UInt32, queue_size=10)
+    pub_volt_2 = rospy.Publisher('tx_volt_2', UInt32, queue_size=10)
 
     rospy.init_node('tx_voltage_publisher', anonymous=True)
     rate = rospy.Rate(0.1)  # 0.1hz 10s period
 
     while not rospy.is_shutdown():
 
-        volt0 = open('/sys/bus/i2c/drivers/ina3221x/0-0040/iio_device/in_voltage0_input').read()
-        volt1 = open('/sys/bus/i2c/drivers/ina3221x/0-0040/iio_device/in_voltage1_input').read()
-        volt2 = open('/sys/bus/i2c/drivers/ina3221x/0-0040/iio_device/in_voltage2_input').read()
+        volt0 = UInt32(open('/sys/bus/i2c/drivers/ina3221x/0-0040/iio_device/in_voltage0_input').read())
+        volt1 = UInt32(open('/sys/bus/i2c/drivers/ina3221x/0-0040/iio_device/in_voltage1_input').read())
+        volt2 = UInt32(open('/sys/bus/i2c/drivers/ina3221x/0-0040/iio_device/in_voltage2_input').read())
 
         pub_volt_0.publish(volt0)
         pub_volt_1.publish(volt1)
